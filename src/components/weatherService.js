ae28516c878f6449e1db3c4d58a5dd16b0c9a6d8
Kyle Irbing <31706313+tobiasrf22@users.noreply.github.com>
2024-01-16 21:53:57 -0300

    Update weatherService.js

--- src/components/weatherService.js
+++ src/components/weatherService.js
@@ -1,13 +1,13 @@
 const API_KEY = 'cfcf7c70518683077835cb49d67fcc77';
 
 const makeIconUrl = (iconId)=> {
-    console.log(`http://openweathermap.org//img/wn/${iconId}@2x.png`);
-    return `http://openweathermap.org//img/wn/${iconId}@2x.png`;
+    console.log(`https://openweathermap.org//img/wn/${iconId}@2x.png`);
+    return `https://openweathermap.org//img/wn/${iconId}@2x.png`;
 };
 
 
 const getFormattedWeatherData = async (city, units) => {
-    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${API_KEY}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${API_KEY}`;
 
     const data = await fetch(URL)
         .then(res => res.json())
